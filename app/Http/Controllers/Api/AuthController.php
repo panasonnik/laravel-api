712303a82d7aaa133d57625a81e42778e70113dc
panasonikk <grebenyuk1507@gmail.com>
2024-04-26 20:04:23 +0200

    Hash imported

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Hash;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\Auth as AuthRequests;
 
 class AuthController extends Controller
